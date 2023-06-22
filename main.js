d1f521db3233095ade4293f227a6c2dbe14f0502
Azeecord Official <112968737+Azeecord@users.noreply.github.com>
2023-06-22 18:16:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup()
+ { video = createCapture(VIDEO); 
+    video.size(550,500)
+    canvas = createCanvas(550,450)
+    canvas.position(560,100);
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotposses);
+
+}
+
+function draw(){
+    background('black');
+
+} 
+
+function modelLoaded(){
+    console.log("Model Loaded");
+
+}
+function gotposses(results){
+    if( results.length > 0){
+    console.log(results)
+    }
+    
+}
